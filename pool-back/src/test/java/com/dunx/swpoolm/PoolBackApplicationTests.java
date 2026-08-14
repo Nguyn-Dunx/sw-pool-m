37300799155bbec99f6e0859297313f2c1684410
DESKTOP-ODU9UF9\ASUS <hehwoh@gmail.com>
2026-08-14 21:51:03 +0700

    delete "spring.flyway.enabled=false" in PoolBackApplicationTests

--- pool-back/src/test/java/com/dunx/swpoolm/PoolBackApplicationTests.java
+++ pool-back/src/test/java/com/dunx/swpoolm/PoolBackApplicationTests.java
@@ -3,9 +3,7 @@ package com.dunx.swpoolm;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest(properties = {
-        "spring.flyway.enabled=false"
-})
+@SpringBootTest
 class PoolBackApplicationTests {
 
     @Test
